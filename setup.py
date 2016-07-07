616070a98a5888afd68b49a9d6ceb7c89b17a8e3
Kyan <ykyankai@gmail.com>
2016-07-07 22:31:02 +0800

    升级版本号

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='4.5.2',
+    version='4.5.3',
 
     description='A console toolkit for common uses',
     long_description='A console toolkit for common uses, by Kyan',
